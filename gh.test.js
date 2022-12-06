be6bca053ea1acdac0f2690d3b2b9444bbb93c75
Leonid Zhdanov <zhdanov.leon@gmail.com>
2022-12-06 21:31:24 +0400

    task is done

--- gh.test.js
+++ gh.test.js
@@ -1,27 +1,33 @@
+jest.setTimeout(60000);
+
 let page;
 
 beforeEach(async () => {
   page = await browser.newPage();
-  await page.goto("https://github.com/team");
-});
+}, 15000);
 
 afterEach(() => {
+  jest.clearAllTimers();
   page.close();
 });
 
 describe("Github page tests", () => {
+  beforeEach(async () => {
+    await page.goto("https://github.com/team");
+  });
+
   test("The h1 header content'", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
     await page.waitForSelector("h1");
     const title2 = await page.title();
-    expect(title2).toEqual("GitHub: Where the world builds software · GitHub");
-  });
+    expect(title2).toEqual("GitHub: Let’s build from here · GitHub");
+  }, 25000);
 
   test("The first link attribute", async () => {
     const actual = await page.$eval("a", (link) => link.getAttribute("href"));
     expect(actual).toEqual("#start-of-content");
-  });
+  }, 15000);
 
   test("The page contains Sign in button", async () => {
     const btnSelector = ".btn-large-mktg.btn-mktg";
@@ -29,6 +35,32 @@ describe("Github page tests", () => {
       visible: true,
     });
     const actual = await page.$eval(btnSelector, (link) => link.textContent);
-    expect(actual).toContain("Sign up for free");
-  });
+    expect(actual).toContain("Get started with Team");
+  }, 15000);
+});
+
+describe("Github Action page tests", () => {
+  beforeEach(async () => {
+    await page.goto("https://github.com/features/actions");
+  }, 15000);
+
+  test("The h1 header content'", async () => {
+    await page.waitForSelector("h1");
+    const title1 = await page.title();
+    expect(title1).toEqual("Features • GitHub Actions · GitHub");
+  }, 15000);
+
+  test("The first link attribute", async () => {
+    const actual = await page.$eval("a", (link) => link.getAttribute("href"));
+    expect(actual).toEqual("#start-of-content");
+  }, 15000);
+
+  test("The page contains Sign in button", async () => {
+    const btnSelector = ".btn-large-mktg.btn-mktg";
+    await page.waitForSelector(btnSelector, {
+      visible: true,
+    });
+    const actual = await page.$eval(btnSelector, (link) => link.textContent);
+    expect(actual).toContain("Get started with Actions");
+  }, 15000);
 });
--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -2,6 +2,7 @@ module.exports = {
   launch: {
     headless: false,
     defaultViewport: null,
+    slowMo: 1000,
     args: ["--start-maximized"], //— используем максимальный размер окна браузера
   },
 };
